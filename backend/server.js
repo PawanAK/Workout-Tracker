a99f80e820d53aab66e8e3be08e470a882eb8b20
PawanAK <pawanajjark@gmail.com>
2023-11-14 19:34:04 +0530

    new route

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,17 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.json({
-        msg: 'Hello World'
-    });
+//middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
 });
 
-app.listen(process.env.PORT, (req, res) => {
-    console.log('listening on port 4000');
+app.get('/', (req, res) => {
+    res.json({ msg: 'Hello World' });
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('listening on port ', process.env.PORT);
 });
 
