c8ee739e6ec600827a3b1772c9d4dcb9a19fb355
PawanAK <pawanajjark@gmail.com>
2023-11-15 12:14:41 +0530

    Update dependencies and routes for workout tracker app

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,28 +1,38 @@
-const express = require('express');
-const Workout = require('../models/workout');
+const express = require('express')
+const Workout = require('../models/workoutModel')
 
-const router = express.Router();
+const router = express.Router()
 
+// GET all workouts
 router.get('/', (req, res) => {
-    res.json({ msg: "Get all Workouts" })
+  res.json({mssg: 'GET all workouts'})
 })
 
+// GET a single workout
 router.get('/:id', (req, res) => {
-    res.json({ msg: "GET a single workout" })
+  res.json({mssg: 'GET a single workout'})
 })
 
-router.post('/', (req, res) => {
-    const { title, load, req } = req.body;
-
-    res.json({ msg: "POST a new Workout" })
+// POST a new workout
+router.post('/', async (req, res) => {
+  const {title, load, reps} = req.body
+  
+  try {
+    const workout = await Workout.create({title, load, reps})
+    res.status(200).json(workout)
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
 })
 
+// DELETE a workout
 router.delete('/:id', (req, res) => {
-    res.json({ msg: "DELETE a workout" })
+  res.json({mssg: 'DELETE a workout'})
 })
 
+// UPDATE a workout
 router.patch('/:id', (req, res) => {
-    res.json({ msg: "PATCH a workout" })
+  res.json({mssg: 'UPDATE a workout'})
 })
 
-module.exports = router;
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,32 @@
-require('dotenv').config();
-const express = require('express');
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
-const app = express();
+// express app
+const app = express()
 
-//middleware
+// middleware
+app.use(express.json())
+
 app.use((req, res, next) => {
-    console.log(req.path, req.method);
-    next();
-});
+  console.log(req.path, req.method)
+  next()
+})
 
+// routes
 app.use('/api/workouts', workoutRoutes)
 
-app.listen(process.env.PORT, () => {
-    console.log('listening on port ', process.env.PORT);
-});
-
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('connected to database')
+    // listen to port
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  }) 
