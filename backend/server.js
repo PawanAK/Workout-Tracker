e2c9d1e7f1f7afcb6e1a3f863c4dc1ecf50b5ca5
PawanAK <pawanajjark@gmail.com>
2023-11-24 23:02:26 +0530

    Update server.js with PORT constant

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
+const PORT = 3000
 
 // express app
 const app = express()
@@ -23,8 +24,8 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('connected to database')
     // listen to port
-    app.listen(process.env.PORT, () => {
-      console.log('listening for requests on port', process.env.PORT)
+    app.listen(PORT, () => {
+      console.log('listening for requests on port', PORT)
     })
   })
   .catch((err) => {
