32a5518ec8195441961a3f0ee34be17058db0479
PawanAK <pawanajjark@gmail.com>
2024-01-11 16:17:00 +0530

    Fix bug in login functionality

---
+++ backend/controllers/userController.js
@@ -0,0 +1,13 @@
+//login user
+const loginUser = async (req, res) => {
+    res.json({ mssg: 'Login successful' })
+}
+
+
+
+
+//signup user
+
+const signupUser = async (req, res) => {
+    res.json({ mssg: 'Signup successful' })
+}
---
+++ backend/routes/user.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const router = express.Router() 
+
+//Login Route
+router.post('/login', (req, res) => {})
+
+
+//Signup Route
+router.post('/signup', (req, res) => {})
+
+
+module.exports = router
