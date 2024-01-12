3bd6839cfe0837a61fde219a490ba45391659745
PawanAK <pawanajjark@gmail.com>
2024-01-12 11:21:58 +0530

    Update user API route

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/api/workouts', workoutRoutes)
-app.use('/api/users', userRoutes)
+app.use('/api/user', userRoutes)
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
