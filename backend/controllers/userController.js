cdb3d618b005cffdddc89356411d5269f9eca0fe
PawanAK <pawanajjark@gmail.com>
2024-01-15 17:14:45 +0530

    Refactor user login functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,7 +8,19 @@ const createToken = (_id) => {
 
 // login a user
 const loginUser = async (req, res) => {
-    res.json({ mssg: 'login user' })
+    const { email, password } = req.body
+
+    try {
+        const user = await User.login(email, password)
+
+        //create a token
+        const token = createToken(user._id)
+
+        res.status(201).json({ email, token })
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+
 }
 
 
