8d3280fa09e412e14546c0cdceef0b0c2e09a8e6
PawanAK <pawanajjark@gmail.com>
2024-01-15 16:19:56 +0530

    token function in userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,10 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+    return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '7d' })
+}
+
 
 // login a user
 const loginUser = async (req, res) => {
