60b3983b13008b4119b61aa247af5f87b5788833
PawanAK <pawanajjark@gmail.com>
2024-01-15 19:33:15 +0530

    feat: :art: AuthContext

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,31 +1,31 @@
-import { createContext, useReducer } from 'react'
+/* eslint-disable react/prop-types */
+import { createContext, useReducer } from "react";
 
-export const AuthContext = createContext()
+export const AuthContext = createContext();
 
 export const authReducer = (state, action) => {
-    switch (action.type) {
-        case 'LOGIN':
-            return {
-                user: action.payload
-            }
-        case 'LOGOUT':
-            return {
-                user: null
-            }
-        default:
-            return state
-    }
-}
+  switch (action.type) {
+    case "LOGIN":
+      return {
+        user: action.payload,
+      };
+    case "LOGOUT":
+      return {
+        user: null,
+      };
+    default:
+      return state;
+  }
+};
 
 export const AuthContextProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(authReducer, { user: null })
+  const [state, dispatch] = useReducer(authReducer, { user: null });
 
-    console.log("AuthContext State: ", state);
+  console.log("AuthContext State: ", state);
 
-    return (
-        <AuthContext.Provider value={{ ...state, dispatch }}>
-            {children}
-        </AuthContext.Provider>
-    )
-
-}
+  return (
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
