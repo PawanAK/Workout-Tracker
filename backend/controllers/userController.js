b41ee68fb067cabf8328e2d30c179419cce87678
PawanAK <pawanajjark@gmail.com>
2024-01-15 16:23:13 +0530

    Refactor signupUser function to include token creation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -18,7 +18,10 @@ const signupUser = async (req, res) => {
     try {
         const user = await User.signup(email, password)
 
-        res.status(201).json({ email, user })
+        //create a token
+        const token = createToken(user._id)
+
+        res.status(201).json({ email, token })
     } catch (error) {
         res.status(500).json({ error: error.message })
     }
