916ef0412f435be7842c9b4707843f38874b69b7
PawanAK <pawanajjark@gmail.com>
2024-01-16 20:19:45 +0530

    refactor

--- frontend/src/hooks/useSignup.jsx
+++ frontend/src/hooks/useSignup.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useAuthContext } from "./useAuthContext";
+import { useContext } from "react";
 
 export const useSignup = () => {
   const [error, setError] = useState(null);
