666eec5832f18deaa423764eb1c30eed9eaa78af
PawanAK <pawanajjark@gmail.com>
2024-01-17 20:02:15 +0530

    navbar

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 
 const Login = () => {
-  const [email, setEmail] = useState(" ");
-  const [password, setPassword] = useState(" ");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
