52168eab85597aefb67be75571416205086bcc7a
PawanAK <pawanajjark@gmail.com>
2024-01-18 13:39:35 +0530

    Add dispatch to useSignup hook

--- frontend/src/hooks/useSignup.jsx
+++ frontend/src/hooks/useSignup.jsx
@@ -4,6 +4,7 @@ import { useAuthContext } from "./useAuthContext";
 export const useSignup = () => {
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(null);
+  const { dispatch } = useAuthContext();
 
   const signup = async (email, password) => {
     setIsLoading(true);
@@ -14,5 +15,18 @@ export const useSignup = () => {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
     });
+    const json = await response.json();
+
+    if (!response.ok) {
+      setIsLoading(false);
+      setError(json.message);
+    }
+    if (response.ok) {
+      localStorage.setItem("user", JSON.stringify(json));
+
+      dispatch({ type: "LOGIN", payload: json });
+      setIsLoading(false);
+    }
   };
+  return { signup, error, isLoading };
 };
