a5d49fec6f7e91234b744bda1028dcba35a0278c
PawanAK <pawanajjark@gmail.com>
2024-01-19 22:51:14 +0530

    fix: :bug: State Overload

--- frontend/src/hooks/useLogout.jsx
+++ frontend/src/hooks/useLogout.jsx
@@ -1,12 +1,15 @@
 import { useAuthContext } from "./useAuthContext";
+import { useWorkoutsContext } from "./useAuthContext";
 
 export const useLogout = () => {
   const { dispatch } = useAuthContext();
+  const { dispatch: workoutsDispatch } = useWorkoutsContext();
 
   const logout = () => {
     localStorage.removeItem("user");
 
     dispatch({ type: "LOGOUT" });
+    workoutsDispatch({ type: "SET_WORKOUTS", payload: null });
   };
 
   return { logout };
