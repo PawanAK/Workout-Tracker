d391898dcc044d8596e22234f00905ce7a24ca2b
PawanAK <pawanajjark@gmail.com>
2024-01-19 22:35:59 +0530

    added Navigate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 // pages & components
 import Home from "./pages/Home";
@@ -8,15 +9,26 @@ import Navbar from "./components/Navbar";
 import "./index.css";
 
 function App() {
+  const { user } = useAuthContext();
+
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <div className="pages">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
+            <Route
+              path="/"
+              element={user ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/login"
+              element={user ? <Navigate to="/" /> : <Login />}
+            />
+            <Route
+              path="/signup"
+              element={user ? <Navigate to="/" /> : <Signup />}
+            />
           </Routes>
         </div>
       </BrowserRouter>
