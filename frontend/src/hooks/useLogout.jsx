f02fe82ee7306726bd5bb2c1d41b46151ab4d9d6
PawanAK <pawanajjark@gmail.com>
2024-01-19 23:02:41 +0530

    refactor: Logout

--- frontend/src/hooks/useLogout.jsx
+++ frontend/src/hooks/useLogout.jsx
@@ -1,15 +1,17 @@
 import { useAuthContext } from "./useAuthContext";
-import { useWorkoutsContext } from "./useAuthContext";
+import { useWorkoutsContext } from "./useWorkoutsContext";
 
 export const useLogout = () => {
   const { dispatch } = useAuthContext();
-  const { dispatch: workoutsDispatch } = useWorkoutsContext();
+  const { dispatch: dispatchWorkouts } = useWorkoutsContext();
 
   const logout = () => {
+    // remove user from storage
     localStorage.removeItem("user");
 
+    // dispatch logout action
     dispatch({ type: "LOGOUT" });
-    workoutsDispatch({ type: "SET_WORKOUTS", payload: null });
+    dispatchWorkouts({ type: "SET_WORKOUTS", payload: null });
   };
 
   return { logout };
